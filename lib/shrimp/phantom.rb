8e6aff392fa217b9a138841d5f3d0db825ae6577
Gildo Fiorito <fiorito.g@gmail.com>
2015-05-29 19:03:44 +0200

    Update phantom.rb

--- lib/shrimp/phantom.rb
+++ lib/shrimp/phantom.rb
@@ -26,7 +26,7 @@ module Shrimp
   class Phantom
     include Dir::Tmpname
     
-    attr_accessor :source, :configuration, :outfile, :executable
+    attr_accessor :source, :configuration, :outfile_path, :executable
     attr_reader :options, :cookies, :result, :error
     SCRIPT_FILE = File.expand_path('../rasterize.js', __FILE__)
     
